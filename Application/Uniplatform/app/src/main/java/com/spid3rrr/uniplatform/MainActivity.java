35cdeda0afe8db007ee717f271f718638b5cd629
xXDeathusXx <38404399+xXDeathusXx@users.noreply.github.com>
2020-06-20 12:10:12 +0100

    Initialize
    Added web template and new android project.

---
+++ Application/Uniplatform/app/src/main/java/com/spid3rrr/uniplatform/MainActivity.java
@@ -0,0 +1,15 @@
+package com.spid3rrr.uniplatform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
